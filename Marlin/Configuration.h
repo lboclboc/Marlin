2d85e95fe970e057f77a587bb105ef91771e6969
Lars Berntzon <lars.berntzon@cecilia-data.se>
2020-03-29 16:54:03 +0200

    Lowered acceleration to 1000

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -544,7 +544,7 @@
  * Override with M201
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4]]]]
  */
-#define DEFAULT_MAX_ACCELERATION      { 2000, 2000, 50, 2000 }
+#define DEFAULT_MAX_ACCELERATION      { 1000, 1000, 50, 1000 }
 
 /**
  * Default Acceleration (change/s) change = mm/s
